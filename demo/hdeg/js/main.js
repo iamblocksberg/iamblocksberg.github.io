252803eae218455a44c4087953448fff9f65a0d9
iamblocksberg <8385967+iamblocksberg@users.noreply.github.com>
2017-11-16 13:41:30 +0700

    Add files via upload

--- demo/hdeg/js/main.js
+++ demo/hdeg/js/main.js
@@ -8,26 +8,51 @@ $(document).ready(function(){
 
 	// Event
 	var isDrawing = false
-	var currentAsset = ''
+	var currentAssetID = ''
 
 	// Init
 	var gridSize = 32
-	var canvasBGColor = '#fff'
+	var canvasBGColor = '#ddd'
+	var allAssets = [
+		{ id: 1, texture: 'block-dirt' },
+		{ id: 2, texture: 'block-grass' },
+		{ id: 3, texture: 'block-lava' },
+		{ id: 4, texture: 'block-water' },
+	]
 
 	function init() {
 		canvasDOM.width = $('.canvas-container').width()
 		canvasDOM.height = $('.canvas-container').height()
 		canvas.fillStyle = canvasBGColor
 		canvas.fillRect(0, 0, canvasDOM.width, canvasDOM.height)
+
+		// show assets
+		var assetsList = $('#assets-container').html()
+		for (var iAssets = 0; iAssets < allAssets.length; iAssets++) {
+			assetsList += '<div data-asset-id="' + iAssets + '" class="asset" style="background-image: url(image/assets/' + allAssets[iAssets].texture + '.png);"></div>'
+		}
+		$('#assets-container').html(assetsList)
+
 	}
 
 	// Asset
-	$('.asset.draw').on('touchend', function(){
-		currentAsset = $(this).data('asset-name')
-	})
+	function setActiveAsset(elem) {
+		$('.asset').removeClass('active')
+		elem.addClass('active')
+	}
 
-	$('.asset.erase').on('touchend', function(){
-		currentAsset = ""
+	$(document).on('touchend', '.asset', function(){
+		var asset_id = $(this).data('asset-id') + ''
+
+		if (currentAssetID == asset_id) {
+			// deselect this asset
+			currentAssetID = ''
+			$(this).removeClass('active')
+		} else {
+			// set select this asset
+			setActiveAsset( $(this) )
+			currentAssetID = asset_id
+		}
 	})
 
 
@@ -69,10 +94,14 @@ $(document).ready(function(){
 		x = positionSnapGrid(x)
 		y = positionSnapGrid(y)
 
-		if (currentAsset != "") {
+		if (currentAssetID == 'erase') {
+			// Erase
+			canvas.fillStyle = canvasBGColor
+			canvas.fillRect(x, y, gridSize, gridSize)
+		} else if (currentAssetID != '') {
 
 			var texture = new Image()
-			texture.src = 'image/assets/' + currentAsset + '.png'
+			texture.src = 'image/assets/' + allAssets[ parseInt(currentAssetID) ].texture + '.png'
 			texture.onload = function() {
 				canvas.drawImage(
 					texture,
@@ -88,12 +117,7 @@ $(document).ready(function(){
 
 			}
 
-		} else {
-			// Erase
-			canvas.fillStyle = canvasBGColor
-			canvas.fillRect(x, y, gridSize, gridSize)
-		} 
-
+		}
 	}
 
 	function positionToGridPoint(pos) {
@@ -138,6 +162,12 @@ $(document).ready(function(){
 
 /*
 
+[next]
+- grid line
+- select block
+
+
+============================
 set grid point each node
 use grid point dont position
 
