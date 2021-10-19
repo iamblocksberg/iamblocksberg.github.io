d53237531fe024b37f88ee7b1bcdd5fd124aae55
iamblocksberg <8385967+iamblocksberg@users.noreply.github.com>
2021-10-19 17:25:12 +0700

    Add files via upload

---
+++ demo/print-image/js/main.js
@@ -0,0 +1,85 @@
+// ref print: https://htmldom.dev/print-an-image/
+
+// ##### Webcam ##### \\
+// let camera_button = document.querySelector("#start-camera");
+let video = document.querySelector("#video");
+let click_button = document.querySelector("#click-photo");
+let canvas = document.querySelector("#canvas");
+let canvas_image = document.querySelector("#canvas_image");
+
+// camera_button.addEventListener("click", async function () {
+//   let stream = await navigator.mediaDevices.getUserMedia({
+//     video: true,
+//     audio: false,
+//   });
+//   video.srcObject = stream;
+// });
+
+window.onload = async function () {
+  let stream = await navigator.mediaDevices.getUserMedia({
+    video: true,
+    audio: false,
+  });
+  video.srcObject = stream;
+};
+
+click_button.addEventListener("click", function () {
+  canvas.getContext("2d").drawImage(video, 0, 0, canvas.width, canvas.height);
+  canvas_image.src = canvas.toDataURL("image/jpeg");
+  canvas_image.onload = () => {
+    doPrint();
+  };
+  // let image_data_url = canvas.toDataURL("image/jpeg");
+
+  // data url of the image
+  // console.log(image_data_url);
+});
+
+// ##### Print ##### \\
+// document.addEventListener("DOMContentLoaded", function () {
+//   // Query the element
+//   const printBtn = document.getElementById("print");
+
+//   printBtn.addEventListener("click", function () {
+//     // Create a fake iframe
+//   });
+// });
+function doPrint() {
+  const iframe = document.createElement("iframe");
+
+  // Make it hidden
+  iframe.style.height = 0;
+  iframe.style.visibility = "hidden";
+  iframe.style.width = 0;
+
+  // Set the iframe's source
+  iframe.setAttribute("srcdoc", "<html><body></body></html>");
+
+  document.body.appendChild(iframe);
+
+  iframe.contentWindow.addEventListener("afterprint", function () {
+    iframe.parentNode.removeChild(iframe);
+  });
+
+  iframe.addEventListener("load", function () {
+    // Clone the image
+    // const image = document.getElementById("image").cloneNode();
+    // const image = document.createElement("image");
+    // image.src = canvas.toDataURL("image/jpeg");
+    const image = canvas_image.cloneNode();
+    image.style.maxWidth = "100%";
+
+    // Append the image to the iframe's body
+    const body = iframe.contentDocument.body;
+    body.style.textAlign = "center";
+    body.appendChild(image);
+    console.log(image);
+
+    iframe.contentWindow.print();
+    // image.addEventListener("load", function () {
+    //   // Invoke the print when the image is ready
+    //   console.log("load print");
+    //   iframe.contentWindow.print();
+    // });
+  });
+}
